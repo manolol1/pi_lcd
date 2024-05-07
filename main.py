ced68440fd0bf25be3a4ca69c3d6d39cf875dd58
manolol1 <manolol@mailbox.org>
2024-05-07 20:14:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ lcd = lcddriver.lcd()
 while (True):
     dt = datetime.datetime.now()
     
-    lcd.lcd_display_string(dt.time().strftime("%H:%M:%S") + " - " + dt.date, 1)
+    lcd.lcd_display_string(f"{dt.hour:02d}" + ":" + f"{dt.minute:02d}" + " - " + f"{dt.day:02d}" + "." + f"{dt.month:02d}" + "." + f"{dt.year:04d}")
     lcd.lcd_display_string(str(weather.getCurrentTemperature()) + "ßC", 2)
     sleep(1)
